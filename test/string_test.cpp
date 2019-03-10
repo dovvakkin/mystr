df13d7844e31f9a1746e1f75f51f683d72822f0d
dovvakkin <dovvakkin@gmail.com>
2019-03-10 03:37:41 +0300

    add operators test

--- test/string_test.cpp
+++ test/string_test.cpp
@@ -2,7 +2,7 @@
 #include <gmock/gmock.h>
 #include "mystr.h"
 
-TEST(MatrixTest, Constructors) {
+TEST(StringTest, Constructors) {
     mystr a(0);
     mystr b("0");
     mystr c(b);
@@ -15,7 +15,7 @@ TEST(MatrixTest, Constructors) {
     EXPECT_TRUE(d == e);
 }
 
-TEST(MatrixTest, Operators) {
+TEST(StringTest, Operators) {
     mystr a(11);
     mystr b(1);
     mystr c(1);
@@ -23,28 +23,41 @@ TEST(MatrixTest, Operators) {
     b = "11";
     EXPECT_TRUE(b == a);
     EXPECT_FALSE(b != a);
+
     c = b;
     EXPECT_TRUE(a == c);
     EXPECT_FALSE(a != c);
+
     a = "1";
     b = "2";
     c = "12";
     EXPECT_TRUE(c == (a + b));
     EXPECT_FALSE(c != (a + b));
+
     a += b;
     EXPECT_TRUE(c == a);
     EXPECT_FALSE(c != a);
 
     EXPECT_TRUE(c[0] == '1');
-    EXPECT_FALSE(c[0] != '1');
+
     c[0] = '&';
     EXPECT_TRUE(c[0] == '&');
-    EXPECT_FALSE(c[0] != '&');
-
 }
 
-TEST(MatrixTest, Streams) {
-    mystr a(0);
-    mystr b("0");
-    EXPECT_TRUE(a == b);
+TEST(StringTest, Streams) {
+    mystr a(1234);
+    mystr b("Maria <3");
+    std::stringstream ss;
+
+    ss << a;
+    EXPECT_TRUE(ss.str() == "1234");
+    ss.str("");
+    ss.clear(); // Clear state flags.
+
+    ss << b;
+    EXPECT_TRUE(ss.str() == "Maria <3");
+    ss.str("");
+    ss.clear(); // Clear state flags.
+
+
 }
